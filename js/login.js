1a45738e47cde54ba8c7898617cbd7c7aa233901
DESKTOP-9RTNCQK\User <leedongguk0@gmail.com>
2025-08-14 21:31:36 +0900

    [Feat] signup page

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ const btn = document.querySelector(".password-visibility");
 const input = document.querySelector(".password-input");
 const icon = btn.querySelector("img");
 
-btn.addEventListener('click', function () {
+function visibility_handler (input, icon) {
   if(input.type === 'password'){
     input.type = 'text';
     icon.src = "../image/visibility_on.svg";
@@ -10,4 +10,6 @@ btn.addEventListener('click', function () {
     input.type = 'password';
     icon.src = "../image/visibility_off.svg";
   }
-});
+}
+
+btn.addEventListener('click', () => visibility_handler(input, icon));
